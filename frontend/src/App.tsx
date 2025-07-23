f1e927c473aecad6d8d276f9e2f19741cc900534
root <root@dede84fbc996>
2025-07-23 05:08:15 +0000

    🎨 Add frontend code and user interface

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Add more routes here */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
